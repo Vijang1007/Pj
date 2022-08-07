fbcac5fba12326fad3825da8371cc69aaf1c5d25
Vijang1007 <109446727+Vijang1007@users.noreply.github.com>
2022-08-07 11:13:28 +0900

    pj

--- P5/Program.cs
+++ P5/Program.cs
@@ -11,7 +11,114 @@ namespace P5
     {
         static void Main()
         {
+            while(true)
+            {
+                string s1 = ReadLine();
+                switch (s1)
+                {
+                    case "1":
+                        for (int i = 0; i < 5; i++)
+                        {
+                            for (int x = 0; x <= i; x++)
+                            {
+                                Write("*");
+                            }
+                            WriteLine();
+                        }
+                        WriteLine();
+                        break;
 
+                    case "2":
+                        for (int i = 0; i < 5; i++)
+                        {
+                            for (int x = 5; x > i; x--)
+                            {
+                                Write("*");
+                            }
+                            WriteLine();
+                        }
+                        WriteLine();
+                        break;
+
+                    case "3":
+                        for (int i = 0; i < 5; i++)
+                        {
+                            for (int x = 4; x > i; x--)
+                            {
+                                Write(" ");
+                            }
+                            for (int x = 0; x <= i; x++)
+                            {
+                                Write("*");
+                            }
+                            WriteLine();
+                        }
+                        WriteLine();
+                        break;
+
+                    case "4":
+                        for (int i = 0; i < 5; i++)
+                        {
+                            for (int x = 0; x < i; x++)
+                            {
+                                Write(" ");
+                            }
+                            for (int x = 5; x > i; x--)
+                            {
+                                Write("*");
+                            }
+                            WriteLine();
+                        }
+                        WriteLine();
+                        break;
+
+                    case "5":
+                        for (int i = 0; i < 4; i++)
+                        {
+                            for (int x = 3; x > i; x--)
+                            {
+                                Write(" ");
+                            }
+                            for (int x = 0; x < (i * 2) + 1; x++)
+                            {
+                                Write("*");
+                            }
+                            WriteLine();
+                        }
+                        WriteLine();
+                        break;
+
+                    case "6":
+                        for (int i = 0; i < 4; i++)
+                        {
+                            for (int j = 0; j < i; j++)
+                            {
+                                Write(" ");
+                            }
+                            for (int x = 4; x > i; x--)
+                            {
+                                Write("* ");
+                            }
+                            WriteLine();
+                        }
+                        for (int i = 0; i < 3; i++)
+                        {
+                            for (int j = 2; j > i; j--)
+                            {
+                                Write(" ");
+                            }
+                            for (int x = 0; x < i + 2; x++)
+                            {
+                                Write("* ");
+                            }
+                            WriteLine();
+                        }
+                        break;
+
+                    default:
+                        return;
+                }
+            }
         }
     }
 }
---
+++ P6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6
+{
+    class Program
+    {
+        static void Main()
+        {
+            //자료형의 범위
+            WriteLine(byte.MaxValue);
+            WriteLine(byte.MinValue);
+            WriteLine(short.MaxValue);
+            WriteLine(short.MinValue);
+            WriteLine(int.MaxValue);
+            WriteLine(int.MinValue);
+            WriteLine(long.MaxValue);
+            WriteLine(long.MinValue);
+            ReadKey();
+        }
+    }
+}
