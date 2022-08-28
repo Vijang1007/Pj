4ddb6a394c195ced108ea99ff9aaf1bec40abb9f
Junyoung1115 <ijunyeong691@gmail.com>
2022-08-28 10:17:06 +0900

    pp
    p

---
+++ P10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 3, b = 4;
+            int re;
+
+            Add(a, b,out re);
+            Swap(ref a, ref b);
+
+            WriteLine(re);
+            WriteLine(a);
+            WriteLine(b);
+
+            ReadKey();
+        }
+
+        static void Add(int a, int b, out int result)
+        {
+            result = a + b;
+        }
+
+        static void Swap(ref int n1, ref int n2)
+        {
+            int temp;
+            temp = n1;
+            n1 = n2;
+            n2 = temp;
+        }
+    }
+}
